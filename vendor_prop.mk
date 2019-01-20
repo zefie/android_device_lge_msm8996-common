32403851686deae43398c2648a58c6b8e754fa32
Alexis <alexisr245@gmail.com>
2019-01-20 12:53:47 +0100

    refactor: Remove deprecated OpenGLRenderer Configurations

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -132,20 +132,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.gpu.driver=1 \
     ro.sf.compbypass.enable=0
 
-# HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=96 \
-    ro.hwui.layer_cache_size=64 \
-    ro.hwui.r_buffer_cache_size=12 \
-    ro.hwui.path_cache_size=39 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=7 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=2048 \
-    ro.hwui.text_small_cache_height=2048 \
-    ro.hwui.text_large_cache_width=3072 \
-    ro.hwui.text_large_cache_height=2048
-
 # OpenGLES
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610
