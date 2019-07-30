e1b6613726269a80a9b9d01396c8f35d81861bfa
Imaginovskiy <imaginovskiy@kilo-tango.net>
2019-07-30 18:37:12 +0000

    MAKEFILE: Add some livedisplay stuff

--- msm8996.mk
+++ msm8996.mk
@@ -258,7 +258,9 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 
 # Media
 PRODUCT_COPY_FILES += \
