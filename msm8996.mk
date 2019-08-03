4a3e2e7a5508f8f452b7d27a43cd4856f13de555
Michael Bardoň <wolfovelp@gmail.com>
2019-08-03 14:14:55 +0200

    msm8996-common: we dont have qcom wifi chip

--- msm8996.mk
+++ msm8996.mk
@@ -420,7 +420,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Wifi Display
 PRODUCT_PACKAGES += \
