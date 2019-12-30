0b09e982a2258c0c3493e6293d9d7e5e5d3d8e8b
Alin <alinmiron3@gmail.com>
2019-12-30 22:33:33 +0200

    msm8996-common: Fix Safailnet path

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -128,7 +128,8 @@ LOCAL_MODULE       := init.safailnet.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.safailnet.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+LOCAL_VENDOR_MODULE    := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 # testing for logs
