5eebb17b4957c12c58da03cbee874ab917bbde11
Alin <alinmiron3@gmail.com>
2019-12-30 21:03:28 +0200

    msm8996-common: Introduce Safailnet

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -123,6 +123,14 @@ LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.safailnet.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.safailnet.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
+
 # testing for logs
 include $(CLEAR_VARS)
 LOCAL_MODULE       := loggy.sh
