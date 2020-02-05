7cb8862f0f9b93963ee83ec3644f7bf0f7e9331c
Michael Bardoň <wolfovelp@gmail.com>
2020-02-05 22:05:33 +0100

    Revert "msm8996-common: Enable clearkey drm plugin v1.2"
    This reverts commit 4a99326c23927c6ad2839e0ff77cbb5bd2a56707.

--- msm8996.mk
+++ msm8996.mk
@@ -187,7 +187,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey \
     move_widevine_data.sh
 
 # Encryption
