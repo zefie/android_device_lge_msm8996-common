6b38edc448a8d0922631cf894242096c283f5c96
zefie <zefie@zefie.net>
2020-02-19 18:14:20 -0500

    revert wifi@1.3 thing

--- msm8996.mk
+++ msm8996.mk
@@ -443,7 +443,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.3-service \
+    android.hardware.wifi@1.0-service \
     libcld80211 \
     libwpa_client \
     hostapd \
